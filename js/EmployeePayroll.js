f0aea880436ce2dbfd2a59931ee9eb7ad9a44418
tenznz <dhugkar@gmail.com>
2022-01-13 09:37:37 -0800

    Added date and name validation

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,22 +1,56 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    salaryOutput();
+    validateName();
+    validateDate();
 
-    const name = document.querySelector('#name');
-    const textError = document.querySelector('.text-error');
+});
+
+function validateName() {
+    const name = document.querySelector("#name");
+    const textError = document.querySelector(".text-error");
     name.addEventListener('input', function() {
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if (nameRegex.test(name.value)) {
+        if (name.value.length == 0) {
             textError.textContent = "";
-        } else {
-            let errorMsg = "Name is invalid";
-            textError.textContent = errorMsg;
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent = "";
+        } catch (e) {
+            console.error(e)
+            textError.textContent = e;
         }
     });
+}
 
+function checkDate() {
+    console.log("checking Date");
+    const dateError = document.querySelector('#startDate');
+    try {
+        let date = day.value + " " + month.value + " " + year.value;
+        (new EmployeePayrollData()).startDate = new Date(Date.parse(date));
+        dateError.innerHTML = "";
+    } catch (e) {
+        console.error(e);
+        dateError.innerHTML = e;
+    }
+}
+
+function validateDate() {
+    console.log("validating Date");
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
+    day.addEventListener('input', checkDate);
+    month.addEventListener('input', checkDate);
+    year.addEventListener('input', checkDate);
+
+}
+const salaryOutput = () => {
     const salary = document.querySelector('#salary');
     const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
     salary.addEventListener('input', function() {
         output.textContent = salary.value;
     });
-
-});
+}
--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -1,4 +1,8 @@
 class EmployeePayrollData {
+
+    constructor() {
+
+    }
     get id() { return this._id; }
     set id(id) { this._id = id; }
 
@@ -12,7 +16,7 @@ class EmployeePayrollData {
 
     get profilePic() { return this._profilePic; }
     set profilePic(profilePic) {
-        this._gender = gender;
+        this._profilePic = profilePic;
     }
 
     get department() { return this._department; }
@@ -27,7 +31,16 @@ class EmployeePayrollData {
     set note(note) { this._note = note; }
 
     get startDate() { return this._startDate; }
-    set startDate(startDate) { this._startDate = startDate; }
+    set startDate(startDate) {
+        let curDate = new Date();
+        if (startDate > curDate)
+            throw "Start date is future date";
+        var diff = Math.abs(curDate.getTime() - startDate.getTime());
+        console.log(diff);
+        if (diff / (1000 * 60 * 60 * 24) > 30)
+            throw "Start date is beyond 30 days";
+        this._startDate = startDate;
+    }
 
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
