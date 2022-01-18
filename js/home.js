6edefce7b2a92bc9744b0b849d1e8a2f211c6c70
tenznz <dhugkar@gmail.com>
2022-01-18 06:18:48 -0800

    fetching data from local storage

--- js/home.js
+++ js/home.js
@@ -1,26 +1,47 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeeDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
 });
-
+const getEmployeeDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    const innerHtml = `${headerHtml}
-           <tr>
-                <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"></td>
-                <td>Tenzing Dhugkar</td>
-                <td>Male</td>
+    if (empPayrollList.length == 0) return;
+
+    let innerHtml = `${headerHtml}`;
+
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}   
+            <tr>
+                <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+                <td>${empPayrollData._name}</td>
+                <td>${empPayrollData._gender}</td>
                 <td>
-                    <div class='dept-label'>Engineer</div>
-                    <div class='dept-label'>HR</div>
+                    <div class='dept-label'>${getDepartment(empPayrollData._department)}</div>
                 </td>
-                <td>450000</td>
-                <td>1 Nov 2021</td>
+                <td>${empPayrollData._salary}</td>
+                <td>${stringifyDate(empPayrollData._startDate)}</td>
                 <td>
                     <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
                     <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
                 </td>
            </tr>        
-    `;
-    document.querySelector('#table-display').innerHTML = innerHtml;
+         `;
+        document.querySelector('#table-display').innerHTML = innerHtml;
+    }
+}
+const getDepartment = (deptList) => {
+    let deptHtml = '';
+    for (let dept of deptList) {
+        deptHtml = `${deptHtml} <div class="dept-label">${dept}</div>`
+    }
+    return deptHtml;
+}
+const stringifyDate = (date) => {
+    const options = { day: 'numeric', month: 'short', year: 'numeric' };
+    const newDate = !date ? "undefined" : new Date(date).toLocaleDateString('en-GB', options);
+    return newDate;
 }
